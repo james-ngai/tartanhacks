c641dd3ebe11132d7973c016c4cb213a8c2c9ca2
James <79486681+james-ngai@users.noreply.github.com>
2023-02-04 10:36:13 -0500

    Init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, signInWithPopup, GoogleAuthProvider, } from "firebase/auth";
-// signOut, signInWithPopup, GoogleAuthProvider,
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import './styles.css';
+import { 
+  hideLoginError, 
+  showLoginState, 
+  showLoginForm, 
+  showApp, 
+  showLoginError, 
+  btnLogin,
+  btnSignup,
+  btnLogout
+} from './ui'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+import { initializeApp } from 'firebase/app';
+import { 
+  getAuth,
+  onAuthStateChanged, 
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  connectAuthEmulator
+} from 'firebase/auth';
+
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyAN_ejK504JVWMvlhqd5NeKNnFIUAMknB0",
   authDomain: "cmu-marketplace-ff7e1.firebaseapp.com",
   projectId: "cmu-marketplace-ff7e1",
@@ -16,37 +28,132 @@ const firebaseConfig = {
   messagingSenderId: "760520518038",
   appId: "1:760520518038:web:2cbe202b4ec1b7572a95b7",
   measurementId: "G-FZ3T02DPV1"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app); 
+// Login using email/password
+const loginEmailPassword = async () => {
+  const loginEmail = txtEmail.value
+  const loginPassword = txtPassword.value
 
-const provider = new GoogleAuthProvider();
-// provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+  // step 1: try doing this w/o error handling, and then add try/catch
+  await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
 
-  signOut(auth).then(() => {
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
+  // step 2: add error handling
+  // try {
+  //   await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+  // }
+  // catch(error) {
+  //   console.log(`There was an error: ${error}`)
+  //   showLoginError(error)
+  // }
+}
+
+// Create new account using email/password
+const createAccount = async () => {
+  const email = txtEmail.value
+  const password = txtPassword.value
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password)
+  }
+  catch(error) {
+    console.log(`There was an error: ${error}`)
+    showLoginError(error)
+  } 
+}
+
+// Monitor auth state
+const monitorAuthState = async () => {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      console.log(user)
+      showApp()
+      showLoginState(user)
+
+      hideLoginError()
+      hideLinkError()
+    }
+    else {
+      showLoginForm()
+      lblAuthState.innerHTML = `You're not logged in.`
+    }
+  })
+}
+
+// Log out
+const logout = async () => {
+  await signOut(auth);
+}
+
+btnLogin.addEventListener("click", loginEmailPassword) 
+btnSignup.addEventListener("click", createAccount)
+btnLogout.addEventListener("click", logout)
+
+const auth = getAuth(firebaseApp);
+connectAuthEmulator(auth, "http://localhost:9099");
+
+monitorAuthState();
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getAuth, signInWithPopup, connectAuthEmulator, GoogleAuthProvider} from "firebase/auth";
+// // signOut, signInWithPopup, GoogleAuthProvider,
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAN_ejK504JVWMvlhqd5NeKNnFIUAMknB0",
+//   authDomain: "cmu-marketplace-ff7e1.firebaseapp.com",
+//   projectId: "cmu-marketplace-ff7e1",
+//   storageBucket: "cmu-marketplace-ff7e1.appspot.com",
+//   messagingSenderId: "760520518038",
+//   appId: "1:760520518038:web:2cbe202b4ec1b7572a95b7",
+//   measurementId: "G-FZ3T02DPV1"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const auth = getAuth(app); 
+// const provider = new GoogleAuthProvider();
+// var firebase = require('firebase');
+// var firebaseui = require('firebaseui');
+// // Initialize the FirebaseUI Widget using Firebase.
+// var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// ui.start('#firebaseui-auth-container', {
+//   signInOptions: [
+//     // List of OAuth providers supported.
+//     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//   ],
+//   // Other config options...
+// });
+// // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+// signInWithPopup(auth, provider)
+//   .then((result) => {
+//     // This gives you a Google Access Token. You can use it to access the Google API.
+//     const credential = GoogleAuthProvider.credentialFromResult(result);
+//     const token = credential.accessToken;
+//     // The signed-in user info.
+//     const user = result.user;
+//     // IdP data available using getAdditionalUserInfo(result)
+//     // ...
+//   }).catch((error) => {
+//     // Handle Errors here.
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // The email of the user's account used.
+//     const email = error.customData.email;
+//     // The AuthCredential type that was used.
+//     const credential = GoogleAuthProvider.credentialFromError(error);
+//     // ...
+//   });
+
+//   signOut(auth).then(() => {
+//     // Sign-out successful.
+//   }).catch((error) => {
+//     // An error happened.
+//   });
